49eda19f1a49d6d2b7ae8bf5923e99ab53db0a62
dwiirfan187 <64444092+dwiirfan187@users.noreply.github.com>
2020-05-27 19:31:16 +0700

    Add files via upload

---
+++ TELE-BOT.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+clear
+sleep 1
+echo "\033[1;31mSELAMAT DATANG DI KUMPULAN BOT TELEGRAM"
+sleep 1
+echo "BISA INFORMASIKAN NAMA ANDA SIAPA ? "
+sleep 1
+read -p "MASUKAN DULU NAMA KK YA :  " name
+sleep 1
+echo "\033[1;33mHALO KAK" $name "NAMA ANDA SUDAH KAMI KETAHUI"
+sleep 1
+echo "SEKARANG KK LOGIN ID DAN UNTUK MASUK KE PROGAM NYA"
+sleep 1
+echo  "MASUKAN ID YANG UDAH DI KASIH ADMIN"
+nama="DWIXIRFAN"
+while [ "$siapa" != "$nama" ]
+do
+   read -p "ID : " siapa
+done
+   echo "\033[1;34msip benar"
+sleep 1
+clear
+echo "YUHU BISA MASUK KE PROGAM NYA"
+sleep 1
+echo "🔍PLEASE WAIT"
+sleep 1
+echo "."
+sleep 1
+echo "."
+sleep 1
+echo "."
+clear
+sleep 1
+sleep 2
+echo "\033[1;32mHY.. SELAMAT DATANG DI KUMPULAN BOT TELEGRAM"
+sleep 1
+echo "KALIAN BEBAS KOK MILIH YANG MANA BOT NYA"
+sleep 1
+echo "MAAF JIKA KURANG YA PASTINYA AKAN DI KEMBANGIN YA"
+sleep 2
+clear
+sleep 1
+echo "\033[1;34m#########################################"
+echo "AUTHOR.............:............DWI IRFAN"
+echo "GITHUB  :  https://github.com/dwiirfan187"
+echo "CONTAC FB............:..........DWI IRFAN"
+echo "CONTAC WA          :         083869932775"
+echo "#########################################"
+echo ""
+sleep 2
+echo "SILAHKAN PILIH BOT YG MAU DI GUNAKAN"
+echo " \033[1;37m[1] \033[1;35mBOT Dana Mision "
+echo " \033[1;37m[2] \033[1;34mBOT GRATIS DANA "
+echo " \033[1;37m[3] \033[1;32mBOT Viuly BOT "
+echo " \033[1;37m[4] \033[1;35mBOT DOGE CLICK"
+echo " \033[1;37m[5] \033[1;33mBOT BEST FREE LTC "
+echo " \033[1;37m[00] \033[0;36mEXIT "
+read -p " {?} MASUKAN PILIHAN ANDA : " pilih
+if [ $pilih = "1" ]
+then
+   echo "\033[1;35mPLEASE WAIT"
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 2
+   echo "MEMBUAT LINK"
+   sleep 3
+   echo "SILAHKAN KALIAN COPAS/COPY PASTE LINK DI BAWAH INI YA BRO"
+   echo ""
+   echo "https://t.me/danaadsmision_bot?start=r0061834278"
+elif [ $pilih = "2" ]
+then
+    echo "\033[1;34mPLEASE WAIT"
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 2
+   echo "MEMBUAT LINK"
+   sleep 3
+   echo "SILAHKAN KALIAN COPAS YA LINK NYA"
+   echo ""
+   echo "https://t.me/Garkahbot?start=r04743472720"
+elif [ $pilih = "3" ]
+then
+   echo "\033[1;32mPLEASE WAIT"
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 2
+   echo "MEMBUAT LINK"
+   sleep 3
+   echo "JANGAN LUPA DI COPAS LINK NYA"
+   echo ""
+   echo "https://t.me/Freeviuly_bot?start=r04743472720"
+elif [ $pilih = "4" ]
+then
+   echo "\033[1;34mPLEASE WAIT"
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 2
+   echo "MEMBUAT LINK"
+   sleep 3
+   echo "JANGAN LUPA DI COPAS YA LINK NYA"
+   echo ""
+   echo "https://t.me/Dogecoin_click_bot?start=dIA3"
+elif [ $pilih = "5" ]
+then
+   echo "\033[1;31mPLEASE WAIT"
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 2
+   echo "MEMBUAT LINK"
+   sleep 3
+   echo "JANGAN LUPA DI COPAS YA LINK"
+   echo ""
+   echo "https://t.me/Best_Free_LTC_Bot?start=r0061834278"
+elif [ $pilih = "00" ]
+then
+    echo "\033[0;31mPLEASE WAIT"
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 1
+   echo "."
+   sleep 2
+   echo "MENGELUARKAN DARI SEMUA SESI"
+   sleep 3
+   echo "\033[1;37mSIP LAH KELUAR"
+   sleep 5
+fi
